75b31314d088d7734701ae9c297ceccd0c86f25d
Erik Lunna <eslunna@gmail.com>
2016-11-23 22:35:21 -0500

    Linted card some more, removed peek().

--- card.py
+++ card.py
@@ -4,7 +4,6 @@
 SUITS = ('c', 'd', 'h', 's')
 FACECARDS = {'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14, 'Z': 15}
 RANKS = dict({str(x): x for x in range(2, 10)}, **FACECARDS)
-HIDDEN = 'Xx'
 
 
 class Card(object):
@@ -12,47 +11,30 @@ class Card(object):
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit.lower()
-        self.hidden = True  # Hide cards by default
 
     def __str__(self):
-        """
-        Returns the colored string representation
-        """
-        if self.hidden:
-            return HIDDEN
-        else:
-            return self.rank + self.suit
+        """ Returns the colored string representation """
+        return self.rank + self.suit
 
     def __eq__(self, other):
-        """
-        Returns True if this card is equal to the other card, False otherwise.
-        """
+        """ Returns True if this card is equal to the other card, False otherwise. """
         return self.rank == other.rank and self.suit == other.suit
 
     def __gt__(self, other):
-        """
-        Returns True if this card's rank is greater than the other card, False otherwise.
+        """ Returns True if this card's rank is greater than the other card,
+            False otherwise.
         """
         return RANKS[self.rank] > RANKS[other.rank]
 
     def __lt__(self, other):
-        """
-        Returns True if this card's rank is lesser than the other card, False otherwise.
+        """ Returns True if this card's rank is lesser than the other card, False
+            otherwise.
         """
         return RANKS[self.rank] < RANKS[other.rank]
 
     def val(self):
-        """
-        Returns the value of the Cards rank.
-        """
+        """ Returns the value of the Cards rank.  """
         return RANKS[self.rank]
-
-    def peek(self):
-        """
-        This is how human/hero's are able to view hidden cards.
-        """
-        return self.rank + self.suit
-
 
 # Define the Joker cards
 JOKER1 = Card('Z', 's')
